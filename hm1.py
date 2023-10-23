ad9423f4a7590c57397b9593e8f72799319f2542
zukaIiiiii <95580917+zukaIiiiii@users.noreply.github.com>
2023-10-23 21:56:43 +0400

    Delete hm1.py

--- hm1.py
+++
@@ -1,5 +0,0 @@
-age=int(input("shemoitanet tkveni mamis asaki: "))
-age2=int(input("shemoitane sheni asaki: "))
-age3=int(age + 10)
-
-print("tkveni asaki tkveni mamis asaks agemateba", + int(age3-age2) ,  "it 10 wlis shemdeg ")
